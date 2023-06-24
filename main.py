705310cf16ad030e2d2ef95c49c66b5c133ef709
Monique Tran <mkatiatran@gmail.com>
2023-06-23 22:16:44 -0500

    linked navbar to pages with flask

--- main.py
+++ main.py
@@ -9,19 +9,48 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+recipes = [
+  {
+    "title": "Avocado Turkey Burger",
+    "ingredients": "avocado, turkey",
+    "dietgroup": "halal",
+  },
+  {
+    "title": "Carrot and Avocado Salad",
+    "ingredients": "avocado",
+    "dietgroup": "halal, gluten-free, vegetarian",
+  },
+  {
+   "title": "Kale Pizza",
+    "ingredients": "olive oil",
+    "dietgroup": "halal",
+  },
+];
 # ------------
 # index
 # ------------
 @app.route('/')
-def index():
-	return render_template('hello.html')
+def show_splash():
+	return render_template('index.html')
 
 # ------------
 # book
 # ------------	
-@app.route('/book/')
-def book():
-	return render_template('book.html')
+@app.route('/about-us/')
+def show_about_us():
+	return render_template('about-us.html')
+
+@app.route('/diet-groups/')
+def show_diet_groups():
+	return render_template('diet-groups.html')
+
+@app.route('/recipes/')
+def show_recipes():
+	return render_template('recipes.html')
+
+@app.route('/ingredients/')
+def show_ingredients():
+	return render_template('ingredients.html')
 
 # debug=True to avoid restart the local development server manually after each change to your code. 
 # host='0.0.0.0' to make the server publicly available. 
