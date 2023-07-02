8331b5c659f59f1d2ba9e11f9b1aa6034ada0c29
Thomas Skrovan <thejaryak@gmail.com>
2023-07-02 14:44:18 -0500

    api calls for instance by id

--- backend/main.py
+++ backend/main.py
@@ -27,5 +27,23 @@ def getDietGroup():
 	response = models.schema_for_dietgroup.dump(dietGroupList)
 	return jsonify(response)
 
+@app.route('/recipes/<int:id>', methods=["GET"])
+def getRecipeId(id):
+	recipeList = db.session.query(Recipe).filter_by(id = id)
+	response = models.schema_for_recipe.dump(recipeList)
+	return jsonify(response)
+
+@app.route('/ingredients/<int:id>', methods=["GET"])
+def getIngredientId(id):
+	ingredientList = db.session.query(Ingredient).filter_by(id = id)
+	response = models.schema_for_ingredient.dump(ingredientList)
+	return jsonify(response)
+
+@app.route('/dietgroups/<int:id>', methods=["GET"])
+def getDietGroupId(id):
+	dietgroupList = db.session.query(DietGroup).filter_by(id = id)
+	response = models.schema_for_dietgroup.dump(dietgroupList)
+	return jsonify(response)
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
