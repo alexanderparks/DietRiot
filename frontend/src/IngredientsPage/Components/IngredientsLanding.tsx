421612929393d1685e373c2eafc9c753ccf1d84f
Thomas Skrovan <thejaryak@gmail.com>
2023-07-05 22:03:01 -0500

    added linking from cards to instance pages

--- frontend/src/IngredientsPage/Components/IngredientsLanding.tsx
+++ frontend/src/IngredientsPage/Components/IngredientsLanding.tsx
@@ -145,7 +145,7 @@ function IngredientsLanding() {
                 <Grid container spacing={2} sx={{ marginLeft: 4 }}>
                     {ingredients.slice(startIndex, endIndex).map((ingredient, i) => (
                         <Grid item xs={3} key={i}>
-                            <IngredientsCard img_src={ingredient.image} name={ingredient.title} />
+                            <IngredientsCard id = {ingredient.id} img_src={ingredient.image} name={ingredient.title} />
                         </Grid>
                     ))}
                 </Grid>
