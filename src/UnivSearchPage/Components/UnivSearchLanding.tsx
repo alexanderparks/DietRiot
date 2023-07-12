d983accad2bd667f063619c2bef992d25636ec9e
Heyu Zhou <regina.zhou@utexas.edu>
2023-07-12 17:21:40 -0500

    updated search page

--- src/UnivSearchPage/Components/UnivSearchLanding.tsx
+++ src/UnivSearchPage/Components/UnivSearchLanding.tsx
@@ -3,13 +3,29 @@ import IngArray from "../../IngredientsPage/Components/IngredientInstance";
 import RecipeArray from "../../RecipesPage/Components/RecipeInstance";
 import DietArray from "../../DietGroupsPage/Components/DietGroupInstance";
 import IngredientsCard from "../../IngredientsPage/Components/IngredientsCard";
+import RecipesCard from "../../RecipesPage/Components/RecipesCard";
+import DietCard from "../../DietGroupsPage/Components/DietGroupsCard";
 import React from "react";
 import axios from "axios";
 import Grid from "@mui/material/Grid";
+import "../style/univSearchStyle.css";
+
 function UnivSearchLanding() {
+    console.log("entered file")
     const [search, setSearch] = React.useState("");
     const [didMount, setDidMount] = React.useState(false);
-    console.log("entered file")
+    // reloads the page whenever hit back button
+    React.useEffect(() => {
+      const handlePopstate = () => {
+        window.location.reload();
+      };
+  
+      window.addEventListener("popstate", handlePopstate);
+  
+      return () => {
+        window.removeEventListener("popstate", handlePopstate);
+      };
+    }, []);
     // Get search query
     React.useEffect(() => {
       const my_url = window.location.href;
@@ -21,18 +37,6 @@ function UnivSearchLanding() {
       setDidMount(true);
     }, []);
     
-    let initData: IngArray = {
-        title: "",
-        id: 0,
-        aisle: "",
-        sugars: 0.0,
-        carbs: 0.0,
-        protein: 0.0,
-        calories: 0.0,
-        serving: "",
-        recipes: [],
-        image: "",
-        };
 
     const [recipeData, setRecipeData] = React.useState<RecipeArray[]>([]);
     const [ingData, setIngData] = React.useState<IngArray[]>([]);
@@ -41,21 +45,18 @@ function UnivSearchLanding() {
     //change this?
     const api_url = "http://localhost:5000/";
     
-    React.useEffect(() => {
+    const make_flask_call = (search: string) => {
       if (didMount) {
         const local_url = "http://localhost:3000/search/" + search;
         const url = api_url+ "search/" + search;
   
-        console.log(url);
+        console.log("url: " + url);
         axios
           .get(url)
           .then(function (response) {
-            let recipeData = response.data.recipeList;
-            
+            let recipeData = response.data.recipes;
             let ingData = response.data.ingredients;
-            let dietData = response.data.dietgroupList;
-            setRecipeData(recipeData);
-            setIngData(ingData);
+            let dietData = response.data.dietgroups;
             setIngData(ingData.map((item: any) => ({
               title: item.title,
               image: item.src,
@@ -67,8 +68,25 @@ function UnivSearchLanding() {
               calories: item.calories,
               serving: item.serving,
               recipes: item.recipes,
-          })));
-            setDietData(dietData);
+            })));
+            setRecipeData(recipeData.map((item: any) => ({
+              image: item.src,
+              id: item.id,
+              calories: item.calories,
+              servings: item.servings,
+              recipeLink: item.recipeLink,
+              title: item.title
+            })));
+            setDietData(dietData.map((item: any) => ({
+              id: item.id,
+              recipes:  item.recipes,
+              title: item.title,
+              image: item.src,
+              desc: item.desc,
+              prohibits: item.prohibits,
+              percentage: item.percentage,
+            })));
+            
             setLoading(false);
   
             // handle succes
@@ -81,43 +99,23 @@ function UnivSearchLanding() {
             // always executed
           });
       }
-    }, [search]);
-  
-    /*
-    const getHighlightedText = (text: string, highlight: any) => {
-      // Split on highlight term and include term into parts, ignore case
-      const parts = text.split(new RegExp(`(${highlight})`, "gi"));
-      return (
-        <span>
-          {" "}
-          {parts.map((part: any, i: any) => (
-            <span
-              key={i}
-              style={
-                part.toLowerCase() === highlight.toLowerCase()
-                  ? { fontWeight: "bold" }
-                  : {}
-              }
-            >
-              {part}
-            </span>
-          ))}{" "}
-        </span>
-      );
     };
-  */
+    React.useEffect(() => {
+      make_flask_call(search);
+  }, [search]);
+    
     return (
       <div className="Search">
         <>
         {loading ? (
           console.log("loading")
         ) : (
-          <h1>
+          <h1 style = {{marginLeft:"30px", fontFamily: "Arial"}}>
             Search Results for "{search}"
           </h1>
         )}
   
-        <h3>
+        <h3 className = "names">
           Ingredients
         </h3>
             
@@ -132,7 +130,44 @@ function UnivSearchLanding() {
                                 sugars={ingredient.sugars} 
                                 carbs={ingredient.carbs} 
                                 protein={ingredient.protein}
-                                serving={ingredient.serving}/>
+                                serving={ingredient.serving}
+                                search = {search}/>
+                            </Grid>
+                        ))}
+        </Grid>
+
+        <h3 className = "names">
+          Recipes
+        </h3>
+            
+        <Grid container sx={{ marginLeft: 0, marginRight: 10, paddingRight: 5, paddingLeft: 10}}>
+                        {recipeData.slice().map((recipes, i) => (
+                            <Grid item xs={3} key = {i}>
+                                <RecipesCard 
+                                id = {recipes.id}
+                                img_src = {recipes.image}
+                                name = {recipes.title}
+                                carb = {recipes.calories}
+                                servings = {recipes.servings}/>
+                            </Grid>
+                        ))}
+        </Grid>
+
+        <h3 className = "names">
+          Diet Groups
+        </h3>
+            
+        <Grid container sx={{ marginLeft: 0, marginRight: 10, paddingRight: 5, paddingLeft: 10}}>
+                        {dietData.slice().map((diets, i) => (
+                            <Grid item xs={3} key = {i}>
+                                <DietCard 
+                                id = {diets.id}
+                                img_src = {diets.image}
+                                name = {diets.title}
+                                restrictions = {diets.prohibits}
+                                desc = {diets.desc}
+                                percentage = {diets.percentage}
+                                />
                             </Grid>
                         ))}
         </Grid>
@@ -142,6 +177,4 @@ function UnivSearchLanding() {
   }
   
 
-export {
-    UnivSearchLanding
-};
+export default UnivSearchLanding;
